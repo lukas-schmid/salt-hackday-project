b08e50ded1dc1a6f21053ddcbdc3a33d765e064a
lukas-schmid <schmid.luk@hotmail.com>
2021-03-10 00:36:13 +0100

    backend api tests

--- backend/routes/recipes.test.js
+++ backend/routes/recipes.test.js
@@ -1,7 +1,54 @@
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../app");
+let should = chai.should();
 const { expect } = require("chai");
+const allRecipes = require("./mockAllRecipes.json");
+const singleRecipe = require("./mockSingleRecipe.json");
 
-describe("test-setup", () => {
-  it("testing something", () => {
-    expect(true).to.be.true;
+chai.use(chaiHttp);
+
+describe("/GET recipes", () => {
+  it("it should GET all the recipes", (done) => {
+    chai
+      .request(server)
+      .get("/api/recipes")
+      .send(allRecipes)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        done();
+      });
   });
 });
+
+describe("/GET recipes by ID", () => {
+  it("it should GET a recipe by ID", (done) => {
+    chai
+      .request(server)
+      .get("/api/recipes/716429")
+      .send(singleRecipe)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        done();
+      });
+  });
+});
+
+describe("test wrong endpoint", () => {
+  it("should send 404 when route does not exist", (done) => {
+    chai
+      .request(server)
+      .get("/api/recipe/")
+      .end((err, res) => {
+        res.should.have.status(404);
+        res.body.should.be.a("object");
+        res.body.should.have.property("message").eql("Not Found");
+        done();
+      });
+  });
+});
+
+// test query formatter https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2
+// https://www.digitalocean.com/community/tutorials/test-a-node-restful-api-with-mocha-and-chai

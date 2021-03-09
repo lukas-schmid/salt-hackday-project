1d110361a5a5ac382971cd298dba195949dde8f4
lukas-schmid <schmid.luk@hotmail.com>
2021-03-09 23:07:01 +0100

    backend update

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
+var recipesRouter = require("./routes/recipes");
 
 var app = express();
 
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api", indexRouter);
+app.use("/api", recipesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -29,7 +29,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.send(err);
 });
 
 module.exports = app;
---
+++ backend/routes/recipes.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET all recipes by ingredients. */
+router.get("/recipes/", function (req, res, next) {
+  res.send("Backend for salt-hackday-project --- get spoonacualar data");
+});
+
+/* GET recipes by ID. */
+router.get("/recipes/:id/", function (req, res, next) {
+  const id = req.params.id;
+  res.send(`get recipe by ID - example: ${id}`);
+});
+
+module.exports = router;

28bf953377348ddb710896167c83d75f2d0684ef
lukas-schmid <schmid.luk@hotmail.com>
2021-03-11 15:13:21 +0100

    fetch all recipes frontend done

--- frontend/src/components/recipes/find/SearchRecipeItem.js
+++ frontend/src/components/recipes/find/SearchRecipeItem.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const SearchRecipeItem = ({ searchQuery }) => {
+  const [apiResponse, setApiResponse] = useState([]);
   const convertQuerytoURL = (arr) => {
     return encodeURIComponent(arr?.join(",+"));
   };
@@ -10,28 +11,31 @@ const SearchRecipeItem = ({ searchQuery }) => {
     const encodedQuery = convertQuerytoURL(searchQuery);
     fetch(`http://localhost:8080/api/recipes?ingredients=${encodedQuery}`)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => setApiResponse(data));
   }, [searchQuery]);
 
-  return (
-    <article className="SearchRecipeItem">
-      <div className="SearchRecipeItem__image">
-        <img src="" alt="" />
-      </div>
-      <div className="SearchRecipeItem__info">
-        <p className="SearchRecipeItem__info--name">Name of the recipe</p>
-        <Link to="/recipe/testid">
-          <button
-            onClick={() => console.log(searchQuery)}
-            className="SearchRecipeItem__info--button"
-            type="button"
-          >
-            Open
-          </button>
-        </Link>
-      </div>
-    </article>
-  );
+  return apiResponse.length === 0
+    ? null
+    : apiResponse.map((data) => {
+        return (
+          <article key={data.id} className="SearchRecipeItem">
+            <div className="SearchRecipeItem__image">
+              <img src={data.image} alt={data.title} />
+            </div>
+            <div className="SearchRecipeItem__info">
+              <p className="SearchRecipeItem__info--name">{data.title}</p>
+              <Link to={`/recipe/${data.id}`}>
+                <button
+                  className="SearchRecipeItem__info--button"
+                  type="button"
+                >
+                  Open
+                </button>
+              </Link>
+            </div>
+          </article>
+        );
+      });
 };
 
 export default SearchRecipeItem;

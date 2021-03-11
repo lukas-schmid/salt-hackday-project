97eb25a23ad66e1e25d07045e5c96d763408b7d1
lukas-schmid <schmid.luk@hotmail.com>
2021-03-11 12:52:52 +0100

    update footer

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 import KitchenIcon from "@material-ui/icons/Kitchen";
 import ReceiptIcon from "@material-ui/icons/Receipt";
@@ -7,18 +8,24 @@ import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 const Footer = () => {
   return (
     <footer className="app__footer">
-      <div className="footer__fridge">
-        <KitchenIcon className="footer__fridge--icon" />
-        <p className="footer__fridge--text">Your Fridge</p>
-      </div>
-      <div className="footer__recipes">
-        <ReceiptIcon className="footer__fridge--icon" />
-        <p className="footer__fridge--text">Recipes</p>
-      </div>
-      <div className="footer__shoppingList">
-        <ShoppingCartIcon className="footer__fridge--icon" />
-        <p className="footer__fridge--text">Shopping List</p>
-      </div>
+      <Link to="/fridge">
+        <div className="footer__fridge">
+          <KitchenIcon className="footer__fridge--icon" />
+          <p className="footer__fridge--text">Your Fridge</p>
+        </div>
+      </Link>
+      <Link to="/recipes">
+        <div className="footer__recipes">
+          <ReceiptIcon className="footer__fridge--icon" />
+          <p className="footer__fridge--text">Recipes</p>
+        </div>
+      </Link>
+      <Link to="shoppinglist">
+        <div className="footer__shoppingList">
+          <ShoppingCartIcon className="footer__fridge--icon" />
+          <p className="footer__fridge--text">Shopping List</p>
+        </div>
+      </Link>
     </footer>
   );
 };
--- frontend/src/components/footer/Footer.test.js
+++ frontend/src/components/footer/Footer.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import Footer from "./Footer";
+import { BrowserRouter } from "react-router-dom";
 
 test("Footer", () => {
-  render(<Footer />);
+  render(
+    <BrowserRouter>
+      <Footer />
+    </BrowserRouter>
+  );
   const fridge = screen.getByText(/your fridge/i);
   const shoppingList = screen.getByText(/shopping list/i);
   const recipes = screen.getByText(/recipes/i);
